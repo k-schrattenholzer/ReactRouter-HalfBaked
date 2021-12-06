8c2ee9bd60c9d05bd68bd44c94d874b77df4d8aa
‘Katie <ktschr11@gmail.com>
2021-12-06 11:52:21 -0800

    navLinks for home and books view working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
 // when using Switch, received an error that switch is not exported from react router, and error pointed to use Routes instead
+import Home from './views/Home/Home'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
 import './App.css'
@@ -9,11 +10,19 @@ function App() {
   return (
     <main className="container">
       <Router>
-        <header>Library Catalog</header>
+        <header>
+          Oh gosh - so many books!
+          <NavLink to="/" className="App-link" exact>
+            Home
+          </NavLink>
+          <NavLink to="/books" className="App-link" exact>
+            Books
+          </NavLink>
+        </header>
         <Switch>
           <Route path="/books/:id" exact component={BookDetail} />
           <Route path="/books" exact component={BookList} />
-          {/* <Route path="/" exact component={Home} /> */}
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </main>
