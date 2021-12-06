74a4f9e4d97d0d48435b30d58948a990ebcb9884
‘Katie <ktschr11@gmail.com>
2021-12-06 12:04:06 -0800

    added destructured prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
-// when using Switch, received an error that switch is not exported from react router, and error pointed to use Routes instead
 import Home from './views/Home/Home'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
 import './App.css'
 
 function App() {
-  // TODO: Add routes to books & views
   return (
     <main className="container">
       <Router>
--- src/views/Books/BookDetail.jsx
+++ src/views/Books/BookDetail.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import Book from '../../components/book/Book'
 import { getBookById } from '../../services/books'
 
 function BookDetail() {
-  const id = 1 // TODO: Use id from route
+  const { bookId } = useParams()
   const [book, setBook] = useState(null)
 
   useEffect(() => {
-    getBookById(id).then(({ data }) => setBook(data))
-  }, [id])
+    getBookById(bookId).then(({ data }) => setBook(data))
+  }, [bookId])
 
   if (!book) return <h3>Loading book...</h3>
 
